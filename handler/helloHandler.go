28bcd6334cc450879c0290ab4349bd1b95dcfbd8
B, Nishok A <nishok_anand@optum.com>
2024-07-22 13:04:58 +0530

    added some insight comments

--- handler/helloHandler.go
+++ handler/helloHandler.go
@@ -17,9 +17,11 @@ func NewHello(l *log.Logger) *HelloHandler {
 
 func (h *HelloHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.l.Println("hello")
+	//r.Body implements io.Reader so io.ReadAll can read it
 	d, err := io.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, "Error in HelloHandler", http.StatusBadRequest)
 	}
+	// you get back a byte array, so use a formatter to give it string
 	fmt.Printf("Hello : %s", d)
 }
--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"context"
+	"github.com/nishokbanand/microservices/handler"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
-
-	"github.com/nishokbanand/microservices/handler"
 )
 
 func main() {
 	logger := log.New(os.Stdout, "logger >>", log.Default().Flags())
 	hh := handler.NewHello(logger)
 	gh := handler.NewGoodBye(logger)
+	// we dont use the defaultServeMux (we are better)
 	sm := http.NewServeMux()
+	//Create a handler with method ServeHTTP
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
@@ -33,13 +34,16 @@ func main() {
 			logger.Fatal(err)
 		}
 	}()
+	//use a bufferedChannel (to be safe) because if there is any line that comes after the sig:=<-sigchan, it wont be executed
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, os.Interrupt)
 	signal.Notify(sigchan, os.Kill)
+	// big brain move, channel waits for the message and it is done after the singal notfies
 	sig := <-sigchan
 	logger.Println("recieve terminate", sig)
 	//gracefully shutdown
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	//just cancel this guy so that we get no warnings
 	defer cancel()
 	server.Shutdown(ctx)
 }
