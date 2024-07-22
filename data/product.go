54fdb2e4aa6164213d894d8a4bfd8ce2144e93b4
B, Nishok A <nishok_anand@optum.com>
2024-07-22 16:41:59 +0530

    should add post and put handler

--- data/product.go
+++ data/product.go
@@ -2,12 +2,13 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
 
 type Product struct {
-	ID          string  `json:"id"`
+	ID          int     `json:"id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price"`
@@ -19,6 +20,12 @@ type Product struct {
 
 type Products []*Product
 
+func (p *Product) FromJSON(r io.Reader) error {
+	decoder := json.NewDecoder(r)
+	err := decoder.Decode(p)
+	return err
+}
+
 func (p *Products) ToJSON(wr io.Writer) error {
 	encoder := json.NewEncoder(wr)
 	err := encoder.Encode(p)
@@ -29,9 +36,40 @@ func GetProducts() Products {
 	return products
 }
 
+func AddProduct(p *Product) {
+	id := getNextId()
+	p.ID = id
+	products = append(products, p)
+}
+
+func PutProduct(p *Product) error {
+	idx, err := GetProduct(p.ID)
+	if err != nil {
+		return err
+	}
+	products[idx] = p
+	return nil
+}
+
+func GetProduct(id int) (int, error) {
+	for idx, value := range products {
+		if value.ID == id {
+			return idx, nil
+		}
+	}
+	return -1, ProductNotFound
+}
+
+var ProductNotFound = fmt.Errorf("Product not found")
+
+func getNextId() int {
+	id := products[len(products)-1].ID
+	return id + 1
+}
+
 var products = []*Product{
 	{
-		ID:          "1",
+		ID:          1,
 		Name:        "Latte",
 		Description: "sweet",
 		Price:       4.50,
@@ -40,7 +78,7 @@ var products = []*Product{
 		Updated_on:  time.Now().UTC().String(),
 	},
 	{
-		ID:          "2",
+		ID:          2,
 		Name:        "Espresso",
 		Description: "Dont drink",
 		Price:       2.50,
--- handler/product.go
+++ handler/product.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/nishokbanand/microservices/data"
 )
@@ -19,6 +22,12 @@ func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		p.getRequest(rw, r)
 		return
+	} else if r.Method == http.MethodPost {
+		p.postRequest(rw, r)
+		return
+	} else if r.Method == http.MethodPut {
+		p.putRequest(rw, r)
+		return
 	}
 	//catch all other methods
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -26,10 +35,58 @@ func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 
 func (p *Product) getRequest(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
-	//we use NewEncoder instead of marshal to avoid storing the data in buffer
+	//we use NewEncoder instead of marshal to avoid having to buffer the output to an in memory slice of bytes
 	// d, err :=json.Marshal(lp)
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to Marshal", http.StatusInternalServerError)
 	}
+}
+
+func (p *Product) postRequest(rw http.ResponseWriter, r *http.Request) {
+	//We use NewDecoder instead of unmarshal
+	// d, _ := io.ReadAll(r.Body)
+	// json.Unmarshal(d, &data.Product{})
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to Unmarshall the request", http.StatusBadRequest)
+	}
+	data.AddProduct(prod)
+	p.l.Printf("Added: %#v", prod)
+}
+
+func (p *Product) putRequest(rw http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	regex := regexp.MustCompile(`/([0-9]+)`)
+	group := regex.FindAllStringSubmatch(path, -1)
+	//FindAllStringSubmatch returns a slice of slices where the outerslice represents the matches by order
+	//and the inner slice represents the exact match followed by capture groups
+	//for eg : here the regex matches /[0-9]+ , when the path is usr/product/1234/details/456
+	//the group becomes [[/1234,1234],[/456,456]]
+
+	if group == nil {
+		http.Error(rw, "Error in URI", http.StatusBadRequest)
+	}
+	fmt.Println(group[0])
+	fmt.Println(group[0][1])
+
+	//bounce off if the uri has more than one id
+	if len(group[0]) != 2 {
+		http.Error(rw, "Error in URI", http.StatusBadRequest)
+	}
+	//get the exact match
+	idString := group[0][1]
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(rw, "Error in URI", http.StatusBadRequest)
+	}
+	prod := &data.Product{}
+	err = prod.FromJSON(r.Body)
+	prod.ID = id
+	if err != nil {
+		p.l.Fatal(err)
+		http.Error(rw, "Unable to Unmarshall the request", http.StatusBadRequest)
+	}
+	data.PutProduct(prod)
 }
